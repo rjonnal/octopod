16124d02ee4e32f269ca9c83e743aa42a92cdc89
Ravi Sankar Jonnal <rjonnal@gmail.com>
2015-12-04 07:56:08 -0800

    drusen progress

--- scripts/average_drusen.py
+++ scripts/average_drusen.py
@@ -7,6 +7,11 @@ files = fm.get('hroct',['Carmen'])
 
 
 def save_to_png(fn,image,clims,scale=1.0,boxx=[],boxy=[],dpi=150):
+    fig = layout_for_png(image,clims,scale=scale,boxx=boxx,boxy=boxy,dpi=dpi)
+    fig.savefig(fn)
+    plt.close(fig)
+    
+def layout_for_png(image,clims,scale=1.0,boxx=[],boxy=[],dpi=150):
     sy,sx = image.shape
     sy_inches = float(sy)/float(dpi)*scale
     sx_inches = float(sx)/float(dpi)*scale
@@ -14,12 +19,14 @@ def save_to_png(fn,image,clims,scale=1.0,boxx=[],boxy=[],dpi=150):
     ax = plt.axes([0,0,1,1])
     imh = ax.imshow(image)
     imh.set_clim(clims)
+    imh.set_cmap('gray')
     if len(boxx):
         plt.autoscale(False)
         plt.plot(boxx,boxy,'y-',lw=1)
-    fig.savefig(fn)
-    
+    return fig
 
+
+clims = [8,13]
 for f in files:
 
     wd_name = os.path.splitext(f)[0]+'_working'
@@ -33,39 +40,56 @@ for f in files:
 
         avifn = os.path.join(wd_name,'%s.avi'%druse_id)
         avgfn = os.path.join(wd_name,'%s_average.png'%druse_id)
+#        if os.path.exists(avgfn):
+#            continue
         bscanfn = os.path.join(wd_name,'%s_bscan.png'%druse_id)
 
+        if os.path.exists(avifn):
+            print bscanfn+' exists'
+            continue
+        
+        reference_index = h5['drusen'][druse_id]['reference_index'][0]
         track = h5['drusen'][druse_id]['tracking'][:]
         ntrack,ncol = track.shape
-        idx,x1,x2,y1,y2,goodness = track[0,:]
+        
+        idx,x1,x2,y1,y2,goodness = track[reference_index,:]
+        
         im_sum = h5['processed_data'][0][idx].copy()
         im_sum = im_sum[y1:y2,x1:x2]
-        im_sum = np.abs(im_sum)
+        im_sum = np.log(np.abs(im_sum))
 
         border = 5
         boxx = [x1-border,x2+border,x2+border,x1-border,x1-border]
-        
         boxy = [y1-border,y1-border,y2+border,y2+border,y1-border]
 
-        reference_index = h5['drusen'][druse_id]['reference_index'][()]
         bscan = np.log(np.abs(h5['processed_data'][0,reference_index,:,:]))
 
-        save_to_png(bscanfn,bscan,(9,12),scale=1.0,boxx=boxx,boxy=boxy)
-        print bscanfn
-        continue
-        
+        save_to_png(bscanfn,bscan,clims,scale=1.0,boxx=boxx,boxy=boxy)
+                
+        mov = Movie(avifn,cmin=clims[0],cmax=clims[1],make_wmv=False)
+        counter = 1.0
         for t in range(1,ntrack):
             print '%s: %d of %d'%(os.path.split(f)[1],t+1,ntrack)
             idx,x1,x2,y1,y2,goodness = track[t,:]
-            im = h5['processed_data'][0][idx].copy()
-            im = im[y1:y2,x1:x2]
-            im = np.abs(im)
-            if im_sum.shape[1]==im.shape[1] and im_sum.shape[0]==im.shape[0]:
-                im_sum = im_sum + im
-            plt.imshow(im,interpolation='none')
-            plt.pause(.1)
-            plt.cla()
-        plt.close()
-        plt.imshow(im_sum,interpolation='none')
-        plt.show()
+            if goodness>0.017:
+                counter = counter + 1
+                im = h5['processed_data'][0][idx].copy()
+                im = im[y1:y2,x1:x2]
+                im = np.log(np.abs(im))
+                if im_sum.shape[1]==im.shape[1] and im_sum.shape[0]==im.shape[0]:
+                    im_sum = im_sum + im
+                fig = layout_for_png(im,clims,scale=5.0)
+                try:
+                    mov.add(fig)
+                except:
+                    pass
+                plt.close(fig)
+                # plt.imshow(im,interpolation='none')
+                # plt.pause(.1)
+                # plt.cla()
+        mov.make()
+        im_sum = im_sum/counter
+        save_to_png(avgfn,im_sum,clims,scale=5.0)
+        #plt.imshow(im_sum,interpolation='none')
+        #plt.show()
 
--- scripts/drusen.py
+++ scripts/drusen.py
@@ -85,7 +85,7 @@ def move_coordinates_in_h5(subject_name):
             h5['drusen'].create_group(key)
             h5['drusen'][key].create_dataset('coordinates',data=arr)
 
-def track_druse_through_set(subject_name,druse_id,border=20):
+def track_druse_through_all_sets(subject_name,druse_id,border=20):
     files = fm.get('hroct')
     files = [f for f in files if f.find(subject_name)>-1]
 
@@ -138,13 +138,62 @@ def track_druse_through_set(subject_name,druse_id,border=20):
             pass
         h5['drusen'][druse_id].create_dataset('tracking',data=np.array(out))
 
+def track_druse_through_set(filename,druse_id,border=20):
+    d = Dataset(filename)
+    h5 = d.get_h5_handle()
+    print h5['drusen'].keys()
+    coords_ref = h5['drusen'][druse_id]['coordinates']
+    xs = coords_ref[:,0]
+    ys = coords_ref[:,1]
+    x1 = round(np.min(xs)-border)
+    x2 = round(np.max(xs)+border)
+    y1 = round(np.min(ys)-border)
+    y2 = round(np.max(ys)+border)
+    refidx = h5['drusen'][druse_id]['reference_index'][0]
+    print refidx
+    temp = h5['processed_data'][0][refidx]
+    template = np.zeros(temp.shape)
+    template[:,x1:x2] = np.abs(temp[:,x1:x2])
+    out = []
+    for k in range(100):
+        target = np.abs(h5['processed_data'][0][k])
+        xshift,yshift,goodness = translation(template,target)
+        d = np.sqrt(xshift**2+yshift**2)
+        print k,d,goodness
+        myx1 = x1 - xshift
+        myx2 = x2 - xshift
+        myy1 = y1 - yshift
+        myy2 = y2 - yshift
+        debug = False
+        if debug:
+            plt.subplot(2,2,1)
+            plt.cla()
+            plt.imshow(template[y1:y2,x1:x2])
+            plt.subplot(2,2,2)
+            plt.cla()
+            plt.imshow(target[myy1:myy2,myx1:myx2])
+            plt.subplot(2,2,3)
+            plt.cla()
+            plt.imshow(template)
+            plt.subplot(2,2,4)
+            plt.cla()
+            plt.imshow(target)
+            plt.pause(.1)
+        out.append([k,myx1,myx2,myy1,myy2,goodness])
+    try:
+        del h5['drusen'][druse_id]['tracking']
+    except:
+        pass
+    h5['drusen'][druse_id].create_dataset('tracking',data=np.array(out))
+        
         
 #label_druse_across_sets('Edric')
 
 carmen_ids = ['1_deg_nasal','0p5_deg_temporal','2_deg_temporal','5_deg_temporal']
 edric_ids = ['0p5_deg_nasal','5_deg_temporal']
 
-for druse_id in carmen_ids:
-    track_druse_through_set('Carmen',druse_id)
-
+files = fm.get('hroct',['Carmen'])
+for f in files:
+    for did in carmen_ids:
+        track_druse_through_set(f,did)
 
