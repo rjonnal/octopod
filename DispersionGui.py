a1061205effe6dc71b3879e9c91c5d1797d6204f
Ravi Sankar Jonnal <rjonnal@gmail.com>
2016-02-10 11:03:14 -0800

    optimization working

--- DispersionGui.py
+++ DispersionGui.py
@@ -8,9 +8,12 @@ from octopod.Processor import process
 from octopod import DispersionOptimizer
 from pyqtgraph import ImageView
 import logging
-from time import time
+from time import time,sleep
 
 logging.basicConfig(level=logging.DEBUG)
+
+Z_CUTOFF = 700
+X_START = 2
 
 class Window(QtGui.QWidget):
     def __init__(self):
@@ -27,11 +30,51 @@ class Window(QtGui.QWidget):
         self.coef_3_label.setText('%s / %0.2e'%(self.coef_3_multiplier_text,self.dispersion_coefs[0]))
         self.coef_2_label.setText('%s / %0.2e'%(self.coef_2_multiplier_text,self.dispersion_coefs[1]))
 
-    def change_coefs(self,newval):
-        self.dispersion_coefs[0] = self.coef_3_text.value() * ocfg.dispersion_3_multiplier
-        self.dispersion_coefs[1] = self.coef_2_text.value() * ocfg.dispersion_2_multiplier
+    def change_coefs(self):
+        new_c3 = self.coef_3_text.value() * ocfg.dispersion_3_multiplier
+        new_c2 = self.coef_2_text.value() * ocfg.dispersion_2_multiplier
+        self.dispersion_coefs[0] = new_c3
+        self.dispersion_coefs[1] = new_c2
+
+        self.coef_3_max = max(self.coef_3_max,new_c3)
+        self.coef_3_min = min(self.coef_3_min,new_c3)
+        self.coef_2_max = max(self.coef_2_max,new_c2)
+        self.coef_2_min = min(self.coef_2_min,new_c2)
+        
         self.show_bscan()
 
+    def optimize(self,obj=np.max,index=0):
+        c3_increment = self.coef_3_step_size*self.coef_3_multiplier
+        c2_increment = self.coef_2_step_size*self.coef_2_multiplier
+        c3_vec = np.arange(self.coef_3_min,self.coef_3_max+c3_increment,c3_increment)
+        c2_vec = np.arange(self.coef_2_min,self.coef_2_max+c2_increment,c2_increment)
+        scores = np.zeros((len(c3_vec),len(c2_vec)))
+        winning_score = -np.inf
+        
+        c3_winner = self.dispersion_coefs[0]
+        c2_winner = self.dispersion_coefs[1]
+
+
+        for idx3,c3 in enumerate(c3_vec):
+            for idx2,c2 in enumerate(c2_vec):
+                coefs = [c3,c2,0.0,0.0]
+                try:
+                    test = self.proc_cache[(index,c3,c2)]
+                except KeyError as ke:
+                    test = np.abs(process(self.raw_vol[index,:,:],self.k_in,self.k_out,coefs)[Z_CUTOFF:,X_START:]).T
+                    self.proc_cache[(index,c3,c2)] = test
+                score = obj(test)
+                scores[idx3,idx2] = score
+                if score>winning_score:
+                    c3_winner = c3
+                    c2_winner = c2
+                plt.imshow(scores,interpolation='none')
+                plt.pause(.1)
+        self.coef_3_text.setValue(c3_winner/self.coef_3_multiplier)
+        self.coef_2_text.setValue(c2_winner/self.coef_2_multiplier)
+        self.change_coefs()
+        self.show_bscan()
+        
     def compute_stats(self):
         try:
             self.bscan_max = np.max(self.bscan)
@@ -56,17 +99,21 @@ class Window(QtGui.QWidget):
         btn_open = QtGui.QPushButton('&open file')
         btn_quit = QtGui.QPushButton('&quit')
         btn_write = QtGui.QPushButton('&write to hdf5')
+        btn_optimize = QtGui.QPushButton('o&ptimize')
         
         btn_open.clicked.connect(self.open_file)
         btn_quit.clicked.connect(self.close)
         btn_write.clicked.connect(self.write_coefs)
+        btn_optimize.clicked.connect(self.optimize)
+
         
         self.coef_3_text = QtGui.QDoubleSpinBox()
         self.coef_2_text = QtGui.QDoubleSpinBox()
 
         self.coef_3_multiplier = ocfg.dispersion_3_multiplier
         self.coef_2_multiplier = ocfg.dispersion_2_multiplier
-
+        self.coef_3_step_size = ocfg.dispersion_3_step_size
+        self.coef_2_step_size = ocfg.dispersion_2_step_size
 
         self.coef_3_multiplier_text = 'x 10^%d'%(np.log10(self.coef_3_multiplier))
         self.coef_2_multiplier_text = 'x 10^%d'%(np.log10(self.coef_2_multiplier))
@@ -74,17 +121,15 @@ class Window(QtGui.QWidget):
 
         self.coef_3_text.setMaximum(ocfg.dispersion_3_max)
         self.coef_3_text.setMinimum(ocfg.dispersion_3_min)
-        self.coef_3_text.setSingleStep(.01)
+        self.coef_3_text.setSingleStep(self.coef_3_step_size)
         
         self.coef_2_text.setMaximum(ocfg.dispersion_2_max)
         self.coef_2_text.setMinimum(ocfg.dispersion_2_min)
-        self.coef_2_text.setSingleStep(.01)
+        self.coef_2_text.setSingleStep(self.coef_2_step_size)
 
         self.coef_3_label = QtGui.QLabel('%0.2e'%self.dispersion_coefs[0])
         self.coef_2_label = QtGui.QLabel('%0.2e'%self.dispersion_coefs[1])
 
-        self.coef_3_text.valueChanged.connect(self.change_coefs)
-        self.coef_2_text.valueChanged.connect(self.change_coefs)
 
         self.stats_label = QtGui.QLabel('')
         
@@ -99,8 +144,9 @@ class Window(QtGui.QWidget):
         layout.addWidget(self.canvas, 0, 0, 5, 9)  # plot goes on right side, spanning 3 rows
         layout.addWidget(self.stats_label, 0, 9, 5, 1)
         layout.addWidget(btn_open, 5, 0, 1, 1)   # button goes in upper-left
-        layout.addWidget(btn_write, 6, 0, 1, 1)   # text edit goes in middle-left
-        layout.addWidget(btn_quit, 7, 0, 1, 1)   # text edit goes in middle-left
+        layout.addWidget(btn_optimize, 6, 0, 1, 1)
+        layout.addWidget(btn_write, 7, 0, 1, 1)   # text edit goes in middle-left
+        layout.addWidget(btn_quit, 8, 0, 1, 1)   # text edit goes in middle-left
         layout.addWidget(self.coef_3_text, 5, 1, 1, 1)  # list widget goes in bottom-left
         layout.addWidget(self.coef_2_text, 6, 1, 1, 1)  # list widget goes in bottom-left
         layout.addWidget(self.coef_3_label, 5, 2, 1, 1)
@@ -124,8 +170,16 @@ class Window(QtGui.QWidget):
             self.k_in = self.h5['k_in'][:]
             self.k_out = self.h5['k_out'][:]
             self.dispersion_coefs = self.h5['dispersion']['coefficients'][:]
+            self.coef_3_min = self.dispersion_coefs[0]
+            self.coef_3_max = self.dispersion_coefs[0]
+            self.coef_2_min = self.dispersion_coefs[1]
+            self.coef_2_max = self.dispersion_coefs[1]
             self.coef_3_text.setValue(self.dispersion_coefs[0]/self.coef_3_multiplier)
             self.coef_2_text.setValue(self.dispersion_coefs[1]/self.coef_2_multiplier)
+
+            self.coef_3_text.valueChanged.connect(self.change_coefs)
+            self.coef_2_text.valueChanged.connect(self.change_coefs)
+
             self.proc_cache = {}
             self.show_bscan()
             
@@ -150,10 +204,10 @@ class Window(QtGui.QWidget):
             
     def show_bscan(self,index=0):
         try:
-            self.bscan = self.proc_cache[(self.dispersion_coefs[0],self.dispersion_coefs[1])]
+            self.bscan = self.proc_cache[(index,self.dispersion_coefs[0],self.dispersion_coefs[1])]
         except KeyError as ke:
-            self.bscan = np.abs(process(self.raw_vol[index,:,:],self.k_in,self.k_out,self.dispersion_coefs)[800:,2:]).T
-            self.proc_cache[(self.dispersion_coefs[0],self.dispersion_coefs[1])] = self.bscan
+            self.bscan = np.abs(process(self.raw_vol[index,:,:],self.k_in,self.k_out,self.dispersion_coefs)[Z_CUTOFF:,X_START:]).T
+            self.proc_cache[(index,self.dispersion_coefs[0],self.dispersion_coefs[1])] = self.bscan
         self.compute_stats()
         self.set_coef_labels()
         self.canvas.setImage(self.bscan)
