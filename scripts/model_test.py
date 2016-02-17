bdf2dcbbfccbe9bd42732ec5ebfd6e8a8639b754
rjonnal <rjonnal@gmail.com>
2016-02-17 15:26:50 -0800

    fixed corner cases in utils.translation that caused goodness values of nan

--- scripts/model_test.py
+++ scripts/model_test.py
@@ -5,4 +5,4 @@ fn = '/home/rjonnal/data/Dropbox/Share/2g_aooct_data/Data/2016.02.02/3T_vid_1.hd
 #fn = '/home/rjonnal/data/Dropbox/Share/2g_aooct_data/Data/2016.02.02/2T_1.hdf5'
 
 model = Model(fn)
-model.make_model(debug=True)
+model.make_model(debug=False)
