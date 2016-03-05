f6d764ec7174cc6864d1d766df66656b889176c0
rjonnal <rjonnal@gmail.com>
2016-03-04 16:02:00 -0800

    calibration work

--- scripts/calibration/compute_calibration_curve.py
+++ scripts/calibration/compute_calibration_curve.py
@@ -15,7 +15,24 @@ im = np.array(Image.open(imfn))
 # ballpark lambda
 L = np.arange(2048)*ocfg.source_spectra['2g_aooct']['dL'] + ocfg.source_spectra['2g_aooct']['L0']
 
+
+im = im - np.min(im)
+
+thresh = 140
+prof = np.mean(im,axis=0)
+
+#prof[np.where(prof<thresh)]=0
+
+
+# load the known peak locations:
+argon_peaks = np.loadtxt('./ocean_optics_argon_lines.txt')*1e-9
+argon_peaks = argon_peaks[np.where(np.logical_and(argon_peaks>np.min(L),argon_peaks<np.max(L)))]
+
 plt.figure()
-plt.plot(L,np.mean(im,axis=0))
+plt.plot(L,prof)
+plt.plot(argon_peaks,np.ones(len(argon_peaks))*np.max(prof),'ks')
+    
+
+
 plt.show()
 
